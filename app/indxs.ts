1c30eb672fc0d498f22bb411cbaae4e1ed504cc8
Pattarapon Janma <pattarapon-j@rmutp.ac.th>
2025-08-27 16:41:55 +0700

    update outpot  scheme

--- app/indxs.ts
+++ app/indxs.ts
@@ -1,13 +1,20 @@
 import { Hono } from "hono";
-
+import { PrismaClient } from "@prisma/client";
+//import { PrismaClient } from "../generated/prisma/client";
+const prisma = new PrismaClient();
 const app = new Hono();
 
 app.get("/", (c) => c.text("Hello World!"));
 
 app.get("/about", (c) => {
-    return c.json ({
-        message: "About Page"
+    return c.json({
+        message: "Pattarapon Janma"
     });
+});
+
+app.get("/profile", async (c) => {
+    const profile = await prisma.profile.findMany();
+    return c.json(profile);
 });
 
 export default app;
