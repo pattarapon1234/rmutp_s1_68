666d8c80a9b1a26a6d8167fa50c235498fe5fd97
Pattarapon Janma <pattarapon-j@rmutp.ac.th>
2025-09-03 13:59:13 +0700

    up lab 9

--- app/indxs.ts
+++ app/indxs.ts
@@ -1,18 +1,14 @@
 import { Hono } from "hono";
-import { PrismaClient } from "@prisma/client";
-//import { PrismaClient } from "../generated/prisma/client";
+import { PrismaClient } from '@prisma/client'
+// import { PrismaClient } from "../generated/prisma/client";
+
 const prisma = new PrismaClient();
+
 const app = new Hono();
-
-app.get("/", (c) => c.text("Hello World!"));
-
-app.get("/about", (c) => {
-    return c.json({
-        message: "Pattarapon Janma"
-    });
-});
-
-app.get("/profile", async (c) => {
+app.get('/', (c) => c.text('Hono!'));
+app.get('/about', (c) => { return c.json({ message: "Pattarapon Janma" }) });
+app.get("/profile", async(c) => {
+    // logic
     const profile = await prisma.profile.findMany();
     return c.json(profile);
 });
