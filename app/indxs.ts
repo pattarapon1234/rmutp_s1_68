e6c77ef16aa342ee71e491953cf781f1fb3f5135
Pattarapon Janma <pattarapon-j@rmutp.ac.th>
2025-09-03 17:08:12 +0700

    change hash with 13 round

--- app/indxs.ts
+++ app/indxs.ts
@@ -1,16 +1,37 @@
 import { Hono } from "hono";
-import { PrismaClient } from '@prisma/client'
-// import { PrismaClient } from "../generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
+import * as bcrypt from "bcrypt";
 
 const prisma = new PrismaClient();
 
 const app = new Hono();
-app.get('/', (c) => c.text('Hono!'));
-app.get('/about', (c) => { return c.json({ message: "Pattarapon Janma" }) });
-app.get("/profile", async(c) => {
-    // logic
-    const profile = await prisma.profile.findMany();
-    return c.json(profile);
+
+app.get("/", (c) => c.text("Hello World Today!"));
+app.get("/profile", async (c) => {
+    //get data from db
+    const profiles = await prisma.profile.findMany();
+    //response
+    return c.json({
+        message: "get data completed",
+        data: profiles
+    }, 200);
+});
+app.post("/profile", async (c) => {
+    //logic to create a new profile
+    const body = await c.req.json();
+    console.log('input of profile ', body);
+    console.log('body.password(original) ', body.password);
+
+    //encode password
+    const passwordHash = await bcrypt.hash(body.password, 18);
+    console.log('hash.password(after) ', passwordHash);
+    body.password = passwordHash;
+    console.log('body.password(replace) ', body);
+    //save to db
+    //output response
+    return c.json({
+        message: "create profile completed"
+    });
 });
 
 export default app;
